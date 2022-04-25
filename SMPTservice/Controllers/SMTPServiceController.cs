cef6b52568f75c49227f8cca7ed19582a7264b91
Jayesh8902 <jayesh.d1998@gmail.com>
2022-04-25 11:15:29 +0530

    Added controller

---
+++ SMPTservice/Controllers/SMTPServiceController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SMPTservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SMTPServiceController : ControllerBase
+    {
+        [Route("SendMail")]
+        [HttpGet]
+        public bool sendMail()
+        { 
+            return false;
+        }
+
+    }
+}
